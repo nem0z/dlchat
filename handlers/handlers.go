c83a9cfcc01625346490aace1620bf3ce6048e51
nem0z <maxime.bernard.dev@gmail.com>
2023-05-02 18:02:42 +0200

    chore: deleted files

--- handlers/handlers.go
+++
@@ -1,5 +0,0 @@
-package handlers
-
-import "github.com/nem0z/dlchat/message"
-
-type Handler func(msg *message.Message) []byte
--- handlers/hello.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import (
-	"github.com/nem0z/dlchat/message"
-)
-
-func Hello(msg *message.Message) bool {
-	return msg.IsValid()
-}
