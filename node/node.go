39b4f48126b7038bb584497c587f4bd5f761d861
nem0z <maxime.bernard.dev@gmail.com>
2023-05-02 14:02:05 +0200

    feat (node): wrap network, storage and keys on struct node

---
+++ node/node.go
@@ -0,0 +1,43 @@
+package node
+
+import (
+	"github.com/nem0z/dlchat/handlers"
+	"github.com/nem0z/dlchat/keys"
+	"github.com/nem0z/dlchat/network"
+	"github.com/nem0z/dlchat/storage"
+)
+
+type Node struct {
+	Network *network.Network
+	Store   *storage.Store
+	Keys    *keys.Keys
+}
+
+func Init(port int, path string, k *keys.Keys) (*Node, error) {
+	network, err := network.Init(port)
+	if err != nil {
+		return nil, err
+	}
+
+	store, err := storage.Init(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if k == nil {
+		k, err = keys.GenerateKeys()
+	}
+
+	network.Register("send", handlers.Send(store))
+	network.Register("fetch", handlers.Fetch(store))
+
+	return &Node{
+		Network: network,
+		Store:   store,
+		Keys:    k,
+	}, nil
+}
+
+func (node *Node) Start() {
+	go node.Network.Start()
+}
