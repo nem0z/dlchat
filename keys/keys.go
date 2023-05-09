07f25c186008d9fc5a525de6d43848fedc61271f
nem0z <maxime.bernard.dev@gmail.com>
2023-05-09 15:30:45 +0200

    feat (keys): export keys

--- keys/keys.go
+++ keys/keys.go
@@ -4,6 +4,10 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"os"
 )
 
 type Keys struct {
@@ -19,4 +23,26 @@ func GenerateKeys() (*Keys, error) {
 	}
 
 	return &Keys{priv, &priv.PublicKey}, nil
+}
+
+func (k *Keys) Export(path string) error {
+	der, err := x509.MarshalECPrivateKey(k.Priv)
+	if err != nil {
+		return err
+	}
+
+	pemBlock := &pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: der,
+	}
+	pemData := pem.EncodeToMemory(pemBlock)
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.Write(pemData)
+	return err
 }
